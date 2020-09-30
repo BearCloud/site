6b5dd69c0cf45eed921c10971e5d6f64474bc6d6
Jie Luo <jieluo@berkeley.edu>
2020-09-29 21:52:35 -0700

    Fix lecture slide link

--- src/components/schedule/Schedule.vue
+++ src/components/schedule/Schedule.vue
@@ -264,7 +264,7 @@ export default {
           "date": "2020-09-29",
           "topic": "Introduction to Databases/SQL",
           "lecture": "",
-          "slides": "lecture7.pdf",
+          "slides": "lecture8.pdf",
           "homework": {
             display: "",
             link: ""
